b08a9669a6009aabb52708bc24dd608fb44af2fd
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-21 22:51:39 +0300

    new changes

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,7 @@
+		-- SQL Script to Create Table
+-- Author: Your Name
+-- Date: 2023-03-21
+
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
