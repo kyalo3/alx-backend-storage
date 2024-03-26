3e8761c29ecc2b18cf77ba36a06a7d23a235d1f7
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-26 22:57:14 +0300

    9. Insert a document in Python

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
-"""function that inserts a new document in a collection """
-from pymongo import MongoClient
+"""function that inserts all documents in a collection """
 
 
 def insert_school(mongo_collection, **kwargs):
-    """ Iterate over all documents in the collection
-    and append them to the list
-    """
-    for document in mongo_collection.find():
-        all_docs.append(document)
-
-    return _id
+    """ return an empty list if no doc is present """
+    return mongo_collection.insert_one(kwargs).inserted_id
