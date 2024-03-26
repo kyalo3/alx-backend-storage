b08f643ca3189dc91888429fa9164bd562f95be6
Kyalo3 <kyalokimeu0@gmail.com>
2024-03-26 22:23:13 +0300

    commenting

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""function that lists all documents in a collection """
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """ return an empty list if no doc is present """
+    if
+    else:
+        return 
+list_all = __import__('8-all').list_all
